ce038d4aee76dd31e7c4c35cc7235860d290b84e
Arkadiy8686 <james.lebron.00@list.ru>
2024-12-14 12:01:20 +0300

    Update calc_operations.py

--- calc_operations.py
+++ calc_operations.py
@@ -24,3 +24,13 @@ def division(a, b):
         raise ValueError("Cannot divide by zero")
     return a / b
 
+def modulus(a, b):
+    a = validate_decimal(a)
+    b = validate_decimal(b)
+    return a % b
+
+def power(a, b):
+    a = validate_decimal(a)
+    b = validate_decimal(b)
+    return a ** b
+
